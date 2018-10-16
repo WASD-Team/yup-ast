40b0e987b218064086fa910f7834bd44bcb71fa5
Will <will@wasd.team>
2018-10-16 15:39:38 +0300

    Added new validations

--- ideas-for-product-create/william/validation.js
+++ ideas-for-product-create/william/validation.js
@@ -37,3 +37,40 @@ const schema = yup.object().shape({
         .required()
         .positive(),
 });
+
+const numberValidation = yup
+    .number()
+    .required()
+    .positive();
+
+const jsonNumberValidation = [['yup.number'], ['yup.required'], ['yup.positive']];
+
+const numberValidationB = yup
+    .number()
+    .required()
+    .positive()
+    .min(50)
+    .max(200);
+
+const jsonNumberValidationB = [['yup.number'], ['yup.required'], ['yup.positive'], ['yup.min', 50], ['yup.max', 200]];
+
+const objectValidation = yup.object().shape({
+    number: yup.number
+        .positive()
+        .min(50)
+        .max(200),
+});
+
+const jsonObjectValidation = [
+    [
+        'yup.object.shape',
+        {
+            number: [
+                ['yup.number'],
+                ['yup.positive'],
+                ['yup.min', 50, 'number.too.small'],
+                ['yup.max', 200, 'number.too.large'],
+            ],
+        },
+    ],
+];
