a3fae25c9ccda3cfe4d86f6aafc6a4a54063ff93
Will <will@wasd.team>
2018-10-17 11:57:19 +0300

    Started work on the convertor

--- source/converter/index.js
+++ source/converter/index.js
@@ -1,11 +1,62 @@
-import yup from 'yup';
+import * as yup from 'yup';
+
+/**
+ * Used when we're unable to find a function.
+ * @returns {boolean} Always false.
+ */
+function defaultValidator() {
+    return false;
+}
+
+/**
+ * Searches for {substring} in {string}.
+ * If found, returns the {string}, sliced after substring.
+ *
+ * @param {string} string - String to be sliced.
+ * @param {string} substring - String to search for.
+ * @returns {string|null} Null if no match found.
+ */
+function getSubString(string, substring) {
+    const testedIndex = string.indexOf(substring);
+
+    if (testedIndex > -1) {
+        return string.slice(testedIndex + substring.length);
+    }
+
+    return null;
+}
+
+/**
+ * Returns a function from yup, by passing in a function name from our schema.
+ * @param {string} functionName - The string to search for a function.
+ * @returns {function} Either the found yup function or the default validator.
+ */
+function getYupFunction(functionName) {
+    const yupName = getSubString(functionName, 'yup.');
+
+    if (yupName) {
+        return yup[yupName];
+    }
+
+    return defaultValidator;
+}
+
+/**
+ * Converts an array of ['yup.number'] to yup.number().
+ * @param {[Any]} jsonArray - The validation array.
+ */
+function convertArray([functionName, ...argsToPass]) {
+    return getYupFunction(functionName)(argsToPass);
+}
 
 /**
  * Takes in a set of json data as defined in our schema, and converts
  * it to a YUP validator.
- * @param {object|array} jsonData - JSON data which will be transformed to yup.
+ * @param {object|array} jsonArray - JSON data which will be transformed to yup.
  * @returns {function} New yup validator
  */
-export function convertJsonToYup(jsonData) {
-    console.log(jsonData);
+export function convertJsonToYup(jsonArray) {
+    const toReturn = convertArray(jsonArray[0]);
+
+    return toReturn;
 }
--- source/converter/tests/converter.test.js
+++ source/converter/tests/converter.test.js
@@ -1,25 +1,30 @@
 import * as yup from 'yup';
 import { convertJsonToYup } from '..';
 
-console.log(yup);
-
-const jsonData = [
+const numberTests = [
     {
         name: 'Converts simple yup type (Number)',
-        input: ['yup.number'],
+        input: [['yup.number']],
         output: yup.number(),
     },
     {
         name: 'Converts simple type with required (Number)',
-        input: ['yup.number', 'yup.required'],
+        input: [['yup.number'], ['yup.required']],
+        output: yup.number().required(),
+    },
+    {
+        name: 'Converts simple type with required chains (Number)',
+        input: [['yup.number'], ['yup.required']],
         output: yup.number().required(),
     },
 ];
 
 describe('correctly transforms data from JSON to YUP', () => {
-    jsonData.forEach(({ name, input, output }) => {
-        it(name, () => {
-            expect(convertJsonToYup(input)).toEqual(output);
+    describe('number tests', () => {
+        numberTests.forEach(({ name, input, output }) => {
+            it(name, () => {
+                expect(convertJsonToYup(input)).toEqual(output);
+            });
         });
     });
 });
